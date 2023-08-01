9f6263753072bb5ce7eb56af3aa509868749cfdd
alemoldoveanu <alemoldoveanu@deloittece.com>
2023-08-01 20:57:48 +0300

    Updating latest exercises to my Alemoldoveanujavaexercises branch.

--- src/main/java/javaLogicExercises/JavaLogicExercises.java
+++ src/main/java/javaLogicExercises/JavaLogicExercises.java
@@ -2,9 +2,47 @@ package javaLogicExercises;
 
 public class JavaLogicExercises {
     public static boolean cigarParty(int cigars, boolean isWeekend) {
-        if (isWeekend && cigars >= 40) {
+        if (isWeekend == true && cigars >= 40) {
             return true;
-        } else return !isWeekend && cigars >= 40 && cigars <= 60;
+        } else if (isWeekend == false && cigars >= 40 && cigars <= 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean love6(int a, int b) {
+        if (a == 6 || b == 6) {
+            return true;
+        } else if (a + b == 6) {
+            return true;
+        } else if (a - b == 6) {
+            return true;
+        } else return b - a == 6;
+    }
+
+    public static int caughtSpeeding(int speed, boolean isBirthday) {
+        if (!isBirthday) {
+            if (speed <= 60) {
+                return 0;
+            } else if (speed <= 80) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            if (speed <= 65) {
+                return 0;
+            } else if (speed <= 85) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+    }
+    public static boolean more20(int n) {
+        return (n % 20 == 1 || n % 20 == 2);
     }
 
 }
+
--- src/main/java/javaLogicExercises/Main.java
+++ src/main/java/javaLogicExercises/Main.java
@@ -3,9 +3,30 @@ package javaLogicExercises;
 public class Main {
     public static void main(String[] args) {
         //When squirrels get together for a party, they like to have cigars.
-        // A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
-        // Unless it is the weekend, in which case there is no upper bound on the number of cigars.
-        // Return true if the party with the given values is successful, or false otherwise.
+        //A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+        //Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+        //Return true if the party with the given values is successful, or false otherwise.
+        //cigarParty(30, false) → false
+        //cigarParty(50, false) → true
+        //cigarParty(70, true) → true
         System.out.println(JavaLogicExercises.cigarParty(50, true));
+        //The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.
+        //love6(6, 4) → true
+        //love6(4, 5) → false
+        //love6(1, 5) → true
+        System.out.println(JavaLogicExercises.love6(1,5));
+        //You are driving a little too fast, and a police officer stops you.
+        //Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket.
+        //If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1.
+        //If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+        //caughtSpeeding(60, false) → 0
+        //caughtSpeeding(65, false) → 1
+        //caughtSpeeding(65, true) → 0*/
+        System.out.println(JavaLogicExercises.caughtSpeeding(85, true));
+        //Return true if the given non-negative number is 1 or 2 more than a multiple of 20.
+        //more20(20) → false
+        //more20(21) → true
+        //more20(22) → true
+        System.out.println(JavaLogicExercises.more20(62));
     }
 }
