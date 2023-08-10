fb2abddc7096f32f1f9f3dd3ab82f54316a3d7f8
alemoldoveanu <alemoldoveanu@deloittece.com>
2023-08-10 09:34:34 +0300

    Updating latest exercises to JavaLogicExercises.

--- src/main/java/javaLogicExercises/JavaLogicExercises.java
+++ src/main/java/javaLogicExercises/JavaLogicExercises.java
@@ -272,6 +272,14 @@ public class JavaLogicExercises {
             return a;
         }
     }
+    public static int forExercise(int[] a){
+        int count = 0;
+        for (int i = 0; i < a.length; i++){
+            if (a[i] % 2 == 0){
+                count++;
+            }
+        } return count;
+    }
 
 }
 
