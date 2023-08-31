497ba9df8a7f5feb4763b585cbd1b5420969e05b
alemoldoveanu <alemoldoveanu@deloittece.com>
2023-08-31 15:44:13 +0300

    Commiting Udemy exercises to repo.

---
+++ src/main/java/udemyExercises/SimpleCalculator.java
@@ -0,0 +1,51 @@
+package udemyExercises;
+
+public class SimpleCalculator {
+
+
+        private double firstNumber = 0.0;
+        private double secondNumber = 0.0;
+
+        public double getFirstNumber(){
+            return firstNumber;
+        }
+
+        public double getSecondNumber(){
+            return secondNumber;
+        }
+
+        public void setFirstNumber(double firstNumber){
+            this.firstNumber = firstNumber;
+        }
+
+        public void setSecondNumber (double secondNumber){
+            this.secondNumber = secondNumber;
+        }
+
+        public double getAdditionResult(){
+            return firstNumber + secondNumber;
+        }
+
+        public double getSubtractionResult(){
+            return firstNumber - secondNumber;
+        }
+
+        public double getMultiplicationResult(){
+            return firstNumber * secondNumber;
+        }
+
+        public double getDivisionResult () {
+
+            if (secondNumber == 0) {
+
+                return this.secondNumber = 0;
+
+            } else {
+
+                return firstNumber / secondNumber;
+
+            }
+
+        }
+}
+
